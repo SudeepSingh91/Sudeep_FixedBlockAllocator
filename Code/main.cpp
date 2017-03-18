69406e04ed2af68f55685b66273bd5cbc377769d
SudeepSingh <u1016400@utah.edu>
2017-03-18 13:37:05 -0600

    Fixed a warning.

--- Code/main.cpp
+++ Code/main.cpp
@@ -13,39 +13,9 @@ void main()
 	//_CrtSetBreakAlloc(79);
 #endif
 	
-	const size_t num = 6;
+	const size_t num = 60;
 	const size_t size = 4;
 	FixedBlockAllocator* m_fixedAllocator = FixedBlockAllocator::Create(num, size);
-
-	float* m1 = static_cast<float*>(m_fixedAllocator->Alloc());
-	*m1 = 4.0f;
-	size_t t1 = m_fixedAllocator->GetTotalFreeMemory();
-
-	float* m2 = static_cast<float*>(m_fixedAllocator->Alloc());
-	*m2 = 4.0f;
-	size_t t2 = m_fixedAllocator->GetTotalFreeMemory();
-
-	float* m3 = static_cast<float*>(m_fixedAllocator->Alloc());
-	*m3 = 4.0f;
-	size_t t3 = m_fixedAllocator->GetTotalFreeMemory();
-
-	float* m4 = static_cast<float*>(m_fixedAllocator->Alloc());
-	*m4 = 4.0f;
-	size_t t4 = m_fixedAllocator->GetTotalFreeMemory();
-
-	m_fixedAllocator->Free(m2);
-	size_t t5 = m_fixedAllocator->GetTotalFreeMemory();
-
-	m_fixedAllocator->Free(m3);
-	size_t t6 = m_fixedAllocator->GetTotalFreeMemory();
-
-	float* m5 = static_cast<float*>(m_fixedAllocator->Alloc());
-	*m5 = 8.0f;
-
-	float* m6 = static_cast<float*>(m_fixedAllocator->Alloc());
-	*m6 = 8.0f;
-
-	float* m7 = static_cast<float*>(m_fixedAllocator->Alloc());
 
 	m_fixedAllocator->Destroy();
 }
